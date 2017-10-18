cb74c3d54570ced220b6cae9f8f8cd639f9f35e3
Joe Kim <jason215.kim@gmail.com>
2017-10-18 15:52:44 -0400

    fix code again
    fix code again

--- src/helloPolarion.java
+++ src/helloPolarion.java
@@ -1,11 +1,8 @@
 /**		
-* @wi.implements drivepilot/DP-528 make link to code
+* @wi drivepilot/DP-528 make link to code
 */
 public String helloFunction(){
-
-
 	String test = "";
 	String mytest = "hello";
-	return test;
-	
+	return test;	
 }
