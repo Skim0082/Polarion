f446b3bea6e63b154630f9aac6b3ddc4c5eb5946
Skim0082 <jason215.kim@gmail.com>
2020-07-16 20:32:21 -0400

    updated

--- src/LinkRoleCockpit.java
+++ src/LinkRoleCockpit.java
@@ -31,7 +31,7 @@ import com.polarion.alm.tracker.model.IWorkItem;
 import com.polarion.platform.core.PlatformContext;
 import com.polarion.platform.persistence.model.IPObjectList;
 /*
-* @wi.implements SPOC-1555 , add java 
+* @wi.implements SPOC-1555 add java 
 */
 public class LinkRoleCockpit extends RichPageWidget {
 	
